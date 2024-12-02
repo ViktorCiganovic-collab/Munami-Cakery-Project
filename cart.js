c4e98a345e72ac012a581fcf5869e66fa190db2e
ViktorCiganovic-collab <dcifw09@hotmail.se>
2024-12-02 15:12:52 +0100

    new images and hover effect for buttons

--- cart.js
+++ cart.js
@@ -64,7 +64,7 @@ document.addEventListener("DOMContentLoaded", () => {
             label.innerHTML = `
                 <h2>Cart is Empty</h2>
                 <a href="product.html">
-                    <button class="HomeBtn">Back to home</button>
+                    <button class="cartBtn">Back to home</button>
                 </a>`;
         }
     };
@@ -78,8 +78,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
             label.innerHTML = `
                 <h2>Total Bill : $ ${amount.toFixed(2)}</h2>
-                <button class="checkout">Back</button>
-                <button onclick="clearCart()" class="removeAll">Clear Cart</button>`;
+                <a href="product.html"><button class="checkout cartBtn">Back</button></a>
+                <button onclick="clearCart()" class="removeAll cartBtn">Clear Cart</button>`;
         }
     };
 
