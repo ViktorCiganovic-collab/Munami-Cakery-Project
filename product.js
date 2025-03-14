ba22a2d914e8ec6e7358984bb7216899d2c93bc2
ViktorCiganovic-collab <dcifw09@hotmail.se>
2025-03-14 12:35:24 +0100

    new productimages

--- product.js
+++ product.js
@@ -10,22 +10,22 @@ const itemContainer = document.querySelector(".cupcakes");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 const products = [
-    { name: "Easy Chocolate Cupcake", category: "Cupcake", image: "https://handletheheat.com/wp-content/uploads/2021/02/chocolate-cupcakes-SQUARE-500x500.png", price: 12.5 },
+    { name: "Easy Chocolate Cupcake", category: "Cupcake", image: "./images/chocolateeasycup.webp", price: 12.5 },
     { name: "Heavenly Chocolate Cupcake", category: "Cupcake", image: "./images/heavenlychoc.jpg", price: 7.5 },
     { name: "Air Fryer Cupcake", category: "Cupcake", image: "https://www.supergoldenbakes.com/wordpress/wp-content/uploads/2023/03/Air_Fryer_Cupcakes-1-4-300x300.jpg", price: 8 },
-    { name: "Rainbow Cupcake", category: "Cupcake", image: "https://www.sugarhero.com/wp-content/uploads/2014/07/exploding-cupcakes-alt-1-sq-featured-image-300x300.jpg", price: 20 },
+    { name: "Rainbow Cupcake", category: "Cupcake", image: "./images/rainbowcupcake.webp", price: 20 },
     { name: "Cupcake Autumn", category: "Cupcake", image: "./images/output (11).jpg", price: 14 },
-    { name: "Lime Cupcake", category: "Cupcake", image: "https://blog.wilton.com/wp-content/uploads/sites/2/2019/05/how-to-make-a-giant-cupcake-feature-500x500.jpg", price: 18 },    
-    { name: "Chocolate Cupcake Peanut Butter", category: "Cupcake", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-FEGWH7FROzJ5b7FwnSfI0q5t6zg1hQsaTw&s", price: 8 },
+    { name: "Lime Cupcake", category: "Cupcake", image: "./images/limecup.jpg", price: 18 },    
+    { name: "Chocolate Cupcake Peanut Butter", category: "Cupcake", image: "./images/peanutcupcake.webp", price: 8 },
     { name: "Strawberry Cupcake", category: "Cupcake", image: "https://i.postimg.cc/nLTZkLjK/output.jpg", price: 20 },
     { name: "Choko Chip Cake", category: "Cake", image: "./images/output (12).jpg", price: 55},
-    { name: "Cherry Wedding Cake", category: "Cake", image: "https://www.cakesbyrobin.co.uk/assets/Naked-Wedding-Cake-300x300.jpg", price: 35},
-    { name: "Lemond Curd Cake", category: "Cake", image: "https://cakeinn.co.uk/wp-content/uploads/nc/62785024/4202644026-300x300.png", price: 42.5},
-    { name: "Three Tier Wedding Cake", category: "Cake", image: "https://benningtonscakes.com/cdn/shop/products/image_71ec4af4-1a58-415c-91d0-d67c83965519_300x300.jpg?v=1638266442", price: 37},
+    { name: "Cherry Wedding Cake", category: "Cake", image: "./images/cheryyweddingcake.webp", price: 35},
+    { name: "Lemon Curd Cake", category: "Cake", image: "./images/LEMON2-scaled.webp", price: 42.5},
+    { name: "Three Tier Wedding Cake", category: "Cake", image: "./images/threetiercake.jpg", price: 37},
     { name: "Wedding Cake", category: "Cake", image: "./images/output (13).jpg", price: 32.5},
-    { name: "Marsipan Cake", category: "Cake", image: "https://isfahansweets.com/wp-content/uploads/2023/07/omani-wedding-cake-300x300.png", price: 57},
-    { name: "Velvet Delight Mini Cake", category: "Cake", image: "https://m.media-amazon.com/images/I/61nWSwQr2FL.jpg", price: 25},
-    { name: "Black Forest Cake", category: "Cake", image: "https://thesweetspotcakes.ca/wp-content/uploads/2023/05/BlackForest-Cake1-300x300.jpg", price: 60}
+    { name: "Marsipan Cake", category: "Cake", image: "./images/marsipancake.webp", price: 57},
+    { name: "Velvet Delight Mini Cake", category: "Cake", image: "./images/81Yz4eelOGL.jpg", price: 25},
+    { name: "Black Forest Cake", category: "Cake", image: "./images/blackforestcake.jpg", price: 60}
 ];
 
 // Set numeric IDs for cupcakes
